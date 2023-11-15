507721e798df1078d78067858a30bbea7c217ca5
Omenesa Muhammed <muhammed.omenesa@microbridge.com.ng>
2023-11-15 11:22:01 +0100

    initial commit

---
+++ src/main/java/com/ohms/mavenspring/MavenspringApplication.java
@@ -0,0 +1,13 @@
+package com.ohms.mavenspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ohms/mavenspring/MavenspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohms.mavenspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
